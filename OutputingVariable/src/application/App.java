5e344c8b4654aba28ae66c7152fee257322ae118
raviranjan1996 <raviranjanp94@gmail.com>
2020-06-19 23:17:42 +0530

    variable

---
+++ OutputingVariable/src/application/App.java
@@ -0,0 +1,26 @@
+package application;
+
+public class App {
+
+	public static void main(String[] args) {
+		int cats = 7;
+		int dogs = 10;
+		int total = cats + dogs;
+		System.out.println("Total  no. of animals " + total);
+
+		double height = 1.8;
+		System.out.println("Hieght is : " + height);
+
+		System.out.println(10.0 / 3);
+
+		dogs = cats;
+		System.out.println(cats);
+		System.out.println(dogs);
+
+		double heights = 1.67;
+		double totalHeight = heights * 7;
+		System.out.println(totalHeight);
+		
+
+	}
+}
