34865349b80a3a7a8f72651f1fb3300e1dd086af
raviranjan1996 <raviranjanp94@gmail.com>
2020-06-23 01:26:59 +0530

    adding format specifire

--- convertingTemperature/src/application/App.java
+++ convertingTemperature/src/application/App.java
@@ -6,11 +6,19 @@ public class App {
 
 	@SuppressWarnings("resource")
 	public static void main(String[] args) {
-		System.out.println("Enter Fehreinheit temperature : ");
+		System.out.println("Enter Fehreinheit temperature -> ");
 		Scanner sc = new Scanner(System.in);
-		double fehrenheit = sc.nextDouble();
+		double fehrenheit = sc.nextFloat();
+		sc.close();
 		double inCelsius = (fehrenheit - 32) * 5.0 / 9.0;
-		System.out.println("Temperature in celsius  is : " + inCelsius + " deg C");
-
+		System.out.printf("%.1f degree Fahrenhiet is %.1f degree celcius. \n", fehrenheit, inCelsius);
+		/*
+		 * double value = 1.234; System.out.printf("hello %.2f\n", value);
+		 * System.out.printf("hello %d\n", 1234);
+		 */
+		
+	
+		
+		
 	}
 }
