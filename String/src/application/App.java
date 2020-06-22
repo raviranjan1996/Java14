544584946d6ab4ac695ceb72d34af81332df6969
raviranjan1996 <raviranjanp94@gmail.com>
2020-06-23 01:30:32 +0530

    string

---
+++ String/src/application/App.java
@@ -0,0 +1,34 @@
+package application;
+
+public class App {
+
+	public static void main(String[] args) {
+		/*
+		 * String is written in capital letter because string is not a primitive
+		 * data type its a java special class;
+		 */
+		
+		String name ="ravi";
+		System.out.println("My name is " + name);
+		
+		String text = "I am called: ";
+		System.out.println(text + name);
+		
+		//controlling text
+		
+		String str = "\tTo be or not to be.\n\tThat is the question";
+		
+		System.out.println(str);
+		
+		String options = "Select an option: \n\t1. Add an entry \n\t2. View the database \n\t3. Exit";
+		System.out.println(options);
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
