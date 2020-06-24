483832069e85549cbeb19e1b9dd018c1d2ccf89a
raviranjan1996 <raviranjanp94@gmail.com>
2020-06-24 16:27:44 +0530

    boolean

---
+++ boolean/src/application/App.java
@@ -0,0 +1,22 @@
+package application;
+
+public class App {
+
+	public static void main(String[] args) {
+
+		boolean condition = true;
+		System.out.println(condition);
+
+		boolean condition1 = 4 < 5;
+		System.out.println(condition1);
+
+		boolean condition2 = 10 > 34;
+		System.out.println(condition2);
+
+		int a = 10, b = 20;
+
+		String input = a < b ? "true" : "false";
+		System.out.println("hey" +input);
+
+	}
+}
