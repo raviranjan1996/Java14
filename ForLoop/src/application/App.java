b74aad1fd147a82f3bff39dedd1c4d86753f437b
raviranjan1996 <raviranjanp94@gmail.com>
2020-06-24 21:01:11 +0530

    ForLoop

---
+++ ForLoop/src/application/App.java
@@ -0,0 +1,18 @@
+package application;
+
+public class App {
+
+	public static void main(String[] args) {
+		
+		String name[] = {"Ravi" , "Ranjan"};
+		for (int i = 0; i < name.length; i++) {
+			System.out.println(name[i]);
+		}
+
+		// infinite for loop
+		/*
+		 * for (;;) { System.out.println("hello"); }
+		 */
+				
+	}
+}
