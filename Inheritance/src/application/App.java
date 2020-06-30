789d8ba9eecdd2b415480c76cb3ebfddb89036cc
raviranjan1996 <raviranjanp94@gmail.com>
2020-07-01 01:20:07 +0530

    inheritence

---
+++ Inheritance/src/application/App.java
@@ -0,0 +1,16 @@
+package application;
+
+public class App {
+
+	public static void main(String[] args) {
+
+		Animal animal = new Animal();
+		animal.eat();
+
+		Bird bird = new Bird();
+		bird.eat();
+		bird.layEgg();
+			//inheritance means inheriting the property of other class into their own class;
+	}
+
+}
