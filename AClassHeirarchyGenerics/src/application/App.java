eaceb4a7738b7ebc1c6c300bc65ddb799167867d
raviranjan1996 <raviranjanp94@gmail.com>
2020-07-25 02:43:30 +0530

    1st commit

---
+++ AClassHeirarchyGenerics/src/application/App.java
@@ -0,0 +1,18 @@
+package application;
+
+import heirarchy.Cat;
+
+public class App {
+
+	public static void main(String[] args) {
+		
+			//generics is a kind of way for creating classes that work with specified type of objects.
+		
+		Cat cat =new Cat("Billi");
+		System.out.println(cat);
+		
+		
+	}
+
+} 
+ 
