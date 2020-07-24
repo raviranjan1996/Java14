10e6576366789ed2d8607adc7bb1996c250864d9
raviranjan1996 <raviranjanp94@gmail.com>
2020-07-25 03:10:45 +0530

    1st commit

---
+++ NullPointException/src/application/App.java
@@ -0,0 +1,13 @@
+package application;
+
+import java.util.Scanner;
+
+public class App {
+
+	public static void main(String[] args) throws Exception{
+	
+		Scanner scanner = null;
+		scanner.nextInt();
+	}
+
+}
