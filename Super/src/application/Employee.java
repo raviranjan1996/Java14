6d32536cdbe3e5a55fd64fd8a94d3454027a7e3b
raviranjan1996 <raviranjanp94@gmail.com>
2020-07-25 02:35:40 +0530

    2nd commit

--- Super/src/application/Employee.java
+++ Super/src/application/Employee.java
@@ -3,7 +3,7 @@ package application;
 public class Employee extends Person {
 	
 	public Employee(String name) {
-		super(name);
+		super(name);    //this will call the super class constructor i.e perspn class 
 		System.out.println("Employee running");
 	}
 
