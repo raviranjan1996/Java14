d6d807cf519133951fcf511a7f2dd087a63f93e7
raviranjan1996 <raviranjanp94@gmail.com>
2020-07-25 02:58:25 +0530

    1st commit

---
+++ Exception/src/application/App.java
@@ -0,0 +1,11 @@
+package application;
+
+public class App {
+	public static void main(String[] args) throws InterruptedException {
+		System.out.println("one");
+		Thread.sleep(2000);              //checked exception
+		System.out.println("two");
+
+	}
+}
+// exception - Any abnormal activity in the program is called exception.
