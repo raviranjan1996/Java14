09e306fa2b868a9bb5b8fc3c99ef7539dc14f39e
raviranjan1996 <raviranjanp94@gmail.com>
2020-08-03 02:53:11 +0530

    LinkedList

---
+++ CollectionLinkedHashSet/src/application/Linkedhashsetintro.java
@@ -0,0 +1,26 @@
+package application;
+
+import java.util.LinkedHashSet;
+
+public class Linkedhashsetintro {
+
+	public static void main(String[] args) {
+		//linkedHashset - ordered , unique , fast , doubly linked list
+		//set.of not used here , because it return a object in unordered way
+		LinkedHashSet<String> set = new LinkedHashSet<String>();
+		set.add("apple");
+		set.add("pear");
+		set.add("cherry");
+		set.add("apple");
+		set.add("mango");
+		for(var fruit:set) {
+			System.out.println(fruit);
+			
+		}
+		
+	
+	
+
+	}
+
+}
